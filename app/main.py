241ce14c7c829fa3cae438cb21fc36ce3c900f09
Ben Slama Mohamed Ali <ben.slama.mohamed.ali@gmail.com>
2025-02-15 17:48:37 +0100

    Add CORS

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -11,6 +12,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = os.environ['JWT_SECRET_KEY']
 app.config['FLASK_ENV'] = 'development'
 app.config['DEBUG'] = True
+
+CORS(app)
 
 # Initialize extensions
 db = SQLAlchemy(app)
